14a92bfe4d6b774371d657c5328d899356e494db
dcfvg <qwsx@dcfvg.com>
2016-07-23 22:13:14 +0200

    test script updt

--- test/test.sh
+++ test/test.sh
@@ -4,9 +4,14 @@ for i in {0..9}
 do
   for j in {0..9}
   do
-    mv="4=0."$i$j
-    echo $mv
-    echo $mv > /dev/pi-blaster
-    sleep 1
+    for k in {0..9}
+    do
+      mv="4=0."$i$j$k
+      echo $mv
+      # echo $mv > /dev/pi-blaster
+      sleep .1
+    done
+    sleep .5
   done
+  sleep 1
 done
