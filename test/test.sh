d7c262113e256e5af78dfff35a553ae960bcd572
dcfvg <qwsx@dcfvg.com>
2016-07-24 15:03:07 +0200

    id in test script

--- test/test.sh
+++ test/test.sh
@@ -1,12 +1,11 @@
 
-
 for i in {0..9}
 do
   for j in {0..9}
   do
     for k in {0..9}
     do
-      mv="4=0."$i$j$k
+      mv="$1=0."$i$j$k
       echo $mv
       echo $mv > /dev/pi-blaster
       sleep .1
@@ -21,6 +20,6 @@ trap onexit SIGTERM
 
 function onexit {
     echo -en "\n## Caught SIGINT; Clean up and Exit \n"
-    echo "4=0" > /dev/pi-blaster
+    echo "$1=0" > /dev/pi-blaster
     exit $?
 }
